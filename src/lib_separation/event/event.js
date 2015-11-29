b5c7654647c646a84ea2718dc4d27819cefa81ca
Tyrannas <someonefefe@gmail.com>
2015-11-29 22:12:45 +0100

    Support de Firefox

--- src/lib_separation/event/event.js
+++ src/lib_separation/event/event.js
@@ -11,7 +11,7 @@ Event.events = {
 	touchend : (appOnDevice_real() ? 'touchend' : 'mouseleave'),
 	dbltap : (appOnDevice_real() ? 'dblclick' : 'dblclick'),
 	hover : (appOnDevice_real() ? '' : 'mousemove'),
-	scroll : (appOnDevice_real() ? '' : 'mousewheel')
+	scroll : (appOnDevice_real() ? '' : (isFirefox)? 'DOMMouseScroll' : 'mousewheel')
 	/*scroll: ( if (appOnDevice_real())
 			 	'' ;
 			 else
